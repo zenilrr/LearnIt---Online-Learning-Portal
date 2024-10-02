56345437cf8f9864fbefe7859cd4ebbc9b891d8d
YashRathod <yashrathod14111@gmail.com>
2024-10-02 19:09:52 +0530

    gentoken and usermodel

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+//this user model is as per the frontend demo made will be changed later
