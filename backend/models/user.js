3a7af6f927ca7fd0652f1f121a6c5541c90b3565
neeltandel <122268045+neeltandel@users.noreply.github.com>
2024-11-11 22:39:49 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,17 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  accountType: {type: String,reuired: true},
-  active: {type: Boolean,default: true,},
-approved: {type: Boolean,default: true,}
-
+const UserSchema = new mongoose.Schema({
+  userName: String,
+  userEmail: String,
+  password: String,
+  role: String,
 });
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
-//this is done for connection perpose
-//this user model is as per the frontend demo made will be changed after
+module.exports = mongoose.model("User", UserSchema);
