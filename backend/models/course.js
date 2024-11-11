b8ee64921a59c4c19bf3355f24cdda6b047ee854
Krutarth Kadia <202201475@daiict.ac.in>
2024-11-11 22:40:48 +0530

    Update course.js

--- backend/models/course.js
+++ backend/models/course.js
@@ -1,58 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
-    courseName: {
-        type: String
-    },
-    courseDescription: {
-        type: String
-    },
-    instructor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    whatYouWillLearn: {
-        type: String
-    },
-    courseContent: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: ''
-            //ref will be updated later 
-        }
-    ],
-    ratingAndReviews: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: ''
-            //will be upated later
-        }
-    ],
-    price: {
-        type: Number
-    },
-    thumbnail: {
-        type: String
-    },
-    studentsEnrolled: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        }
-    ],
-    instructions: {
-        type: [String]
-    },
-    createdAt: {
-        type: Date,
-    }
-    ,
-    updatedAt: {
-        type: Date,
-    }
-
+const LectureSchema = new mongoose.Schema({
+  title: String,
+  videoUrl: String,
+  public_id: String,
+  freePreview: Boolean,
 });
 
-module.exports = mongoose.model('Course', courseSchema);
+const CourseSchema = new mongoose.Schema({
+  instructorId: String,
+  instructorName: String,
+  date: Date,
+  title: String,
+  category: String,
+  level: String,
+  primaryLanguage: String,
+  subtitle: String,
+  description: String,
+  image: String,
+  welcomeMessage: String,
+  pricing: Number,
+  objectives: String,
+  students: [
+    {
+      studentId: String,
+      studentName: String,
+      studentEmail: String,
+      paidAmount: String,
+    },
+  ],
+  curriculum: [LectureSchema],
+  isPublised: Boolean,
+});
+
+module.exports = mongoose.model("Course", CourseSchema);
