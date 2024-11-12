db5425cf1627a4b3673569cbc3a8300a900200ed
Shah Heer Kamleshkumar <202201525@daiict.ac.in>
2024-11-12 22:13:52 +0530

    Update to LoginUser.js

---
+++ backend/controllers/loginUser.js
@@ -0,0 +1,30 @@
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+
+const loginUser = async (req, res) => {
+  const { userEmail, password } = req.body;
+
+  const checkUser = await User.findOne({ userEmail });
+
+  if (!checkUser || !(await bcrypt.compare(password, checkUser.password))) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid credentials",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Logged in successfully",
+    data: {
+      user: {
+        _id: checkUser._id,
+        userName: checkUser.userName,
+        userEmail: checkUser.userEmail,
+        role: checkUser.role,
+      },
+    },
+  });
+};
+
+module.exports = loginUser;
