084fd8c70079766e247d75aceebd40ba44603ae9
impriyank8 <202201470@daiict.ac.in>
2024-11-19 20:03:22 +0530

    BuyNow and StartLearning Page Added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,15 +8,15 @@ import Popup from "./Components/Popup";
 import "./App.css";
 import CreateCourse from "./Components/CreateCourse";
 import FeedbackPage from "./Components/FeedbackPage";
-import LearnPressDemo from "./Components/LearnPressDemo";
 import CoursePage from "./Components/CoursePage";
 import CreateQuiz from "./Components/CreateQuiz";
-
 import HomePage from "./Pages/General/HomePage";
 import InstructorDashboardPage from "./Pages/Instructor/InstructorDashboardPage";
 import SturdentProfilePage from "./Pages/Student/StudentProfilePage";
 import EditProfilePage from "./Pages/General/EditProfilePage";
 import SearchPage from "./Pages/General/SearchPage";
+import BuyNowPage from "./Components/BuyNow"; 
+import StartLearningPage from "./Components/StartLearning";
 
 function App() {
   return (
@@ -26,16 +26,18 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/learnCourse" element={<CoursePage />} />
         <Route path="/instructor-dashboard" element={<InstructorDashboardPage />} />
-        {/* <Route path="/create-course" element={
+         <Route path="/create-course" element={
           <> <Header />
             <CreateCourse />
             <Footer />
-          </>} /> */}
+          </>} /> 
         <Route path="/profile" element={<SturdentProfilePage />} />
         <Route path="/edit-profile" element={<EditProfilePage />} />
         <Route path="/search-courses" element={<SearchPage />} />
         <Route path="/create-quize" element={<CreateQuiz />} />
       </Routes>
+      <BuyNowPage/>
+      {/* <StartLearningPage/> */}
     </div>
   );
 }
